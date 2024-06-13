4ba0c5776ff12662385bbe4d6d73011dbdeaf587
Daniel Kayode <dharnykay@gmail.com>
2024-06-13 22:45:53 +0100

    Add the Email Input Field

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,5 +1,5 @@
 import images from "../assets/assets";
-
+import InputComponent from "./inputField";
 const Signin = () => {
   return (
     <div className="grid place-items-center h-screen">
@@ -7,7 +7,16 @@ const Signin = () => {
         <div className="flex flex-col gap-3 place-items-center gap-1 p-3  ">
           <h1 className="text-[3rem]">Goat Note</h1>
           <h3>Welcome, Literally Go the Extra Mile in Your studies</h3>
-          <form className="flex flex-col gap-2" action=""></form>
+          <form className="flex flex-col gap-2" action="">
+            <InputComponent
+              id="email"
+              name="email"
+              label="Email"
+              type="text"
+              placeholder="abshikah jemmel"
+              className=""
+            />
+          </form>
         </div>
         <div className="h-35 w-40 p-1">
           <img className="w-full h-full" src={images.signinImage} alt="" />

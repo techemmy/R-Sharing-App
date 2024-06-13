27f7ae0756ac35e6cb89e94ecc5b0ede836c6e30
Daniel Kayode <dharnykay@gmail.com>
2024-06-13 22:34:12 +0100

    Imported and Exported Assets

---
+++ src/assets/assets.js
@@ -0,0 +1,27 @@
+import calendar from "./Calendar.png";
+import Chart_fill from "./Chart_fill.png";
+import Chart from "./Chart.png";
+import Chat from "./Chat.png";
+import control from "./control.png";
+import Folder from "./Folder.png";
+import logo from "./logo.png";
+import Search from "./Search.png";
+import Setting from "./Setting.png";
+import User from "./User.png";
+import signinImage from "./Register-Background.png";
+
+const images = {
+  calendar: calendar,
+  Chart_fill: Chart_fill,
+  Chart: Chart,
+  Chat: Chat,
+  control: control,
+  Folder: Folder,
+  logo: logo,
+  Search: Search,
+  Setting: Setting,
+  User: User,
+  signinImage: signinImage,
+};
+
+export default images;

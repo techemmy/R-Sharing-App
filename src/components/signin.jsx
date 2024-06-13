904875352153526e6af8ee8b5c07f1f0179f969d
Daniel Kayode <dharnykay@gmail.com>
2024-06-13 22:37:55 +0100

    Setup Signin Form

---
+++ src/components/signin.jsx
@@ -0,0 +1,20 @@
+import images from "../assets/assets";
+
+const Signin = () => {
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className=" overflow-hidden shadow-2xl items-center flex w-max justify-center border border-gray-300 rounded-xl">
+        <div className="flex flex-col gap-3 place-items-center gap-1 p-3  ">
+          <h1 className="text-[3rem]">Goat Note</h1>
+          <h3>Welcome, Literally Go the Extra Mile in Your studies</h3>
+          <form className="flex flex-col gap-2" action=""></form>
+        </div>
+        <div className="h-35 w-40 p-1">
+          <img className="w-full h-full" src={images.signinImage} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;

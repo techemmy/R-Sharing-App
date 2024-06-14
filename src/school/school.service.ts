2496968bbea88aaa9772c70ddc21beccdfa03e1b
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-14 01:42:13 +0100

    feat(service): implement user service

--- src/school/school.service.ts
+++ src/school/school.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateSchoolDto } from './dto/create-school.dto';
 import { UpdateSchoolDto } from './dto/update-school.dto';
+import { School, SchoolDocument } from './schemas/school.schema';
 
 @Injectable()
 export class SchoolService {
-  create(createSchoolDto: CreateSchoolDto) {
-    return 'This action adds a new school';
+  constructor(@InjectModel(School.name) private schoolModel: Model<School>) {}
+
+  async create(createSchoolDto: CreateSchoolDto): Promise<SchoolDocument> {
+    const { name, acronym } = createSchoolDto;
+    const schoolNameExists = await this.findByName(name);
+    const schoolAcronymExists = await this.findByAcronym(acronym);
+    if (schoolNameExists) {
+      throw new BadRequestException('School name already exists');
+    }
+    if (schoolAcronymExists) {
+      throw new BadRequestException('School acronym already exists');
+    }
+    return this.schoolModel.create(createSchoolDto);
   }
 
-  findAll() {
-    return `This action returns all school`;
+  findAll(): Promise<SchoolDocument[] | null> {
+    return this.schoolModel.find({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} school`;
+  findById(id: string): Promise<SchoolDocument | null> {
+    return this.schoolModel.findById(id);
   }
 
-  update(id: number, updateSchoolDto: UpdateSchoolDto) {
-    return `This action updates a #${id} school`;
+  findByName(name: string): Promise<SchoolDocument | null> {
+    return this.schoolModel.findOne({ name });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} school`;
+  findByAcronym(acronym: string): Promise<SchoolDocument | null> {
+    return this.schoolModel.findOne({ acronym });
+  }
+
+  async update(id: string, updateSchoolDto: UpdateSchoolDto) {
+    const { name, acronym } = updateSchoolDto;
+    const schoolNameExists = await this.findByName(name);
+    const schoolAcronymExists = await this.findByAcronym(acronym);
+    if (schoolNameExists) {
+      throw new BadRequestException('School name already exists');
+    }
+    if (schoolAcronymExists) {
+      throw new BadRequestException('School acronym already exists');
+    }
+    return this.schoolModel.findByIdAndUpdate(id, updateSchoolDto, {
+      new: true,
+    });
+  }
+
+  remove(id: string) {
+    return this.schoolModel.findByIdAndDelete(id);
   }
 }

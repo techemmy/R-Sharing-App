239dccd727152270405c5aee3faae6dbd3afb160
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-14 01:53:17 +0100

    feat(api): configure school schema

--- src/school/school.module.ts
+++ src/school/school.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SchoolService } from './school.service';
 import { SchoolController } from './school.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { School, schoolSchema } from './schemas/school.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: School.name, schema: schoolSchema }]),
+  ],
   controllers: [SchoolController],
-  providers: [SchoolService]
+  providers: [SchoolService],
+  exports: [SchoolService],
 })
 export class SchoolModule {}

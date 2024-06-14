3340718fbd83a56a4d843782101fa45060ca7958
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-14 01:54:54 +0100

    fix(controller): re-implement school controller

--- src/school/school.controller.ts
+++ src/school/school.controller.ts
@@ -1,34 +1,70 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  NotFoundException,
+  BadRequestException,
+  HttpCode,
+} from '@nestjs/common';
 import { SchoolService } from './school.service';
 import { CreateSchoolDto } from './dto/create-school.dto';
 import { UpdateSchoolDto } from './dto/update-school.dto';
 
-@Controller('school')
+@Controller({ version: '1', path: 'schools' })
 export class SchoolController {
   constructor(private readonly schoolService: SchoolService) {}
 
   @Post()
-  create(@Body() createSchoolDto: CreateSchoolDto) {
-    return this.schoolService.create(createSchoolDto);
+  async create(@Body() createSchoolDto: CreateSchoolDto) {
+    const newSchool = await this.schoolService.create(createSchoolDto);
+    return { data: newSchool, message: 'School created successfully' };
   }
 
   @Get()
-  findAll() {
-    return this.schoolService.findAll();
+  async findAll() {
+    const schools = await this.schoolService.findAll();
+    return { data: schools, message: 'Got schools' };
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.schoolService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const school = await this.schoolService.findById(id);
+    if (school == null) {
+      throw new NotFoundException('School not found');
+    }
+    return { data: school, message: 'Got school successfully' };
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSchoolDto: UpdateSchoolDto) {
-    return this.schoolService.update(+id, updateSchoolDto);
+  async update(
+    @Param('id') id: string,
+    @Body() updateSchoolDto: UpdateSchoolDto,
+  ) {
+    const school = await this.schoolService.findById(id);
+    if (school == null) {
+      throw new BadRequestException('School does not exist');
+    }
+    if (school.name === updateSchoolDto.name) {
+      delete updateSchoolDto.name;
+    }
+    if (school.acronym === updateSchoolDto.acronym) {
+      delete updateSchoolDto.acronym;
+    }
+    const updatedSchool = await this.schoolService.update(id, updateSchoolDto);
+    return { data: updatedSchool, message: 'School updated succesfully' };
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.schoolService.remove(+id);
+  async remove(@Param('id') id: string) {
+    const school = await this.schoolService.findById(id);
+    if (school == null) {
+      throw new BadRequestException('School does not exist');
+    }
+    return this.schoolService.remove(id);
   }
 }

8847962bbce4796742a6ab68f6bf6ba2564cbfb0
Daniel Kayode <dharnykay@gmail.com>
2024-06-14 10:47:00 +0100

    Refactor Button to accept className as prop

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,9 +1,5 @@
-const Button = () => {
-  return (
-    <button className="bg-black self-center w-max px-5 py-1 text-white">
-      Log In
-    </button>
-  );
+const Button = ({ label, className }) => {
+  return <button className={className}>{label}</button>;
 };
 
 export default Button;

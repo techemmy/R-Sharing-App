7026ab040b1d52124012bdbdd25f5de69bc04907
Daniel Kayode <dharnykay@gmail.com>
2024-06-16 00:44:34 +0100

    Add IDs to Sidebar array

--- src/components/viewResourceSidebar.jsx
+++ src/components/viewResourceSidebar.jsx
@@ -1,9 +1,24 @@
 import { useState } from "react";
 import images from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = ({ layout }) => {
-  const menus = [{ title: "View Resource" }, { title: "Summary" }];
+  const menus = [
+    { id: 1, title: "View Resource" },
+    { id: 2, title: "Summary" },
+    { id: 3, title: "Back to Home" },
+  ];
 
+  const navigate = useNavigate();
+
+  const handleNavigate = (id) => {
+    if (id === 3) {
+      navigate("/feed"); // Specify the route you want to navigate to
+    } else {
+      // Handle other navigation or actions based on id
+      console.log(`Clicked item with id: ${id}`);
+    }
+  };
   return (
     <div
       className={` ${
@@ -16,6 +31,9 @@ const Sidebar = ({ layout }) => {
         {menus.map((menu, index) => (
           <li
             key={index}
+            onClick={() => {
+              handleNavigate(menu.id);
+            }}
             className={`border-0.1 mb-2  rounded-md p-1.3 text-center cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 `}
           >
             <span className={`${!open && "hidden"} origin-left duration-200`}>

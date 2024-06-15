090c94e846573eda6b95759afa453b0ad2f1769f
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-15 21:44:35 +0100

    style(todo): add TODOs

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { AppModule } from './app.module';
 // TODO: add status codes to controllers
 // TODO: add return types to services
 // TODO: move throwing errors to the controllers
+// TODO: add IsMongooseIdpipe to controllers getting Id
+// TODO: make response structure consistent
+// TODO: limit file upload size
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);

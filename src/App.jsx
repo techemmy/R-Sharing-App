d478ae2ffbc98fd45137dd17224bfa188f6115e4
Daniel Kayode <dharnykay@gmail.com>
2024-06-15 23:59:48 +0100

    Set Navigation Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import Signin from "./components/signin";
+import ViewResourcePage from "./pages/viewResourcePage";
+import HomePage from "./testPagesjsx/homepage";
+import Sidebar from "./components/sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProfilePage from "./components/profileComponent";
 
 const App = () => {
   return (
     <div>
-      <Signin />
+      <Router>
+        <Sidebar />
+        <Routes>
+          <Route path="/feed" element={<HomePage />} />
+          <Route path="/resource-details" element={<ViewResourcePage />} />
+          <Route path="/view-profile" element={<ProfilePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 };

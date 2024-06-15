3225e7820da6ba11ab2818fd9b7c9eb770963aad
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-15 21:52:16 +0100

    feat(decorator): create pagination decorator

---
+++ src/decorators/pagination.decorator.ts
@@ -0,0 +1,31 @@
+import {
+  BadRequestException,
+  createParamDecorator,
+  ExecutionContext,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+export interface Pagination {
+  page: number;
+  limit: number;
+  size: number;
+  offset: number;
+}
+
+export const PaginationParams = createParamDecorator(
+  (data, ctx: ExecutionContext): Pagination => {
+    const req: Request = ctx.switchToHttp().getRequest();
+    const page = parseInt(req.query.page as string) || 1;
+    const size = parseInt(req.query.size as string) || 9;
+
+    if (size > 100) {
+      throw new BadRequestException(
+        'Invalid pagination params: Max size is 100',
+      );
+    }
+
+    const limit = size;
+    const offset = (page - 1) * limit;
+    return { page, limit, size, offset };
+  },
+);

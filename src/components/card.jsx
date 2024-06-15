4ad2084b1ef6bb1329adb13f0fddc87d73ae745e
Daniel Kayode <dharnykay@gmail.com>
2024-06-15 23:34:27 +0100

    Add Course Details star and unstar to card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,12 +2,23 @@ const Card = ({ label, image, id }) => {
   return (
     <>
       <div className="shadow-3xl flex items-center border-0.3 border-gray-400 rounded-3xl gap-2  w-80">
-        <img src={image} alt="" className="bg-black" />
-        <div>
+        <img src={image} alt="" className="w-20" />
+        <div className="flex flex-col gap-2">
           <p>
             <small>{id}</small>
           </p>
           <h1 className="">{label}</h1>
+          <div className="flex gap-1">
+            <button className="bg-black  w-max px-5 py-1 text-white">
+              View Resource
+            </button>
+            <button className="bg-black  w-max px-2 py-1 text-white">
+              Star
+            </button>
+            <button className="bg-black  w-max px-2 py-1 text-white">
+              Unstar
+            </button>
+          </div>
         </div>
       </div>
     </>

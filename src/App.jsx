f4dcf92e6b7743bf2ee91a7d2925e47560aa1db5
Daniel Kayode <dharnykay@gmail.com>
2024-06-16 21:36:55 +0100

    Redefine Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
-import Signin from "./components/signin";
+import Signin from "./pages/signin";
 import ViewResourcePage from "./pages/viewResourcePage";
 import HomePage from "./testPagesjsx/homepage";
 import Sidebar from "./components/sidebar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import ProfilePage from "./components/profileComponent";
+import SummaryComponent from "./components/summaryComponent";
+import Registration from "./pages/signupForm";
+import LandingPage from "./pages/landingPage";
+import { signUpAction } from "./actions";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route exact path="/" element={<LandingPage />}></Route>
+      <Route path="/home" element={<HomePage />}></Route>
+      <Route path="/login" element={<Signin />}></Route>
+      <Route path="/signup" element={<Registration />}></Route>
+    </>
+  )
+);
 
 const App = () => {
-  return (
-    <div>
-      <Router>
-        <Sidebar />
-        <Routes>
-          <Route path="/feed" element={<HomePage />} />
-          <Route path="/resource-details" element={<ViewResourcePage />} />
-          <Route path="/view-profile" element={<ProfilePage />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// <Router>
+//         <Sidebar />
+//         <Routes>
+//           <Route path="/feed" element={<HomePage />} />
+
+//           <Route path="/resource-details" element={<ViewResourcePage />} />
+
+//           <Route path="/signin" element={<Signin />} />
+//           <Route path="/signup" element={<Registration />} />
+//         </Routes>
+//       </Router>

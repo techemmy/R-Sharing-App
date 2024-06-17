9d89c543b5631b813a9491a9b7a1d7df9702aa50
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-17 10:56:02 +0100

    feat(provider): setup auth provider for authentication

---
+++ src/provider/authProvider.jsx
@@ -0,0 +1,51 @@
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import api from "../api/index";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [token, setToken_] = useState(localStorage.getItem("token"));
+
+  const setToken = (newToken) => {
+    setToken_(newToken);
+  };
+
+  useEffect(() => {
+    if (token) {
+      api.defaults.headers.common["Authorization"] = "Bearer " + token;
+      localStorage.setItem("token", token);
+    } else {
+      delete api.defaults.headers.common["Authorization"];
+      localStorage.removeItem("token");
+    }
+  }, [token]);
+
+  const contextValue = useMemo(
+    () => ({
+      token,
+      setToken,
+    }),
+    [token]
+  );
+
+  return (
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const ProtectedRoute = () => {
+  const { token } = useAuth();
+
+  if (!token) {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
+
+export default AuthProvider;

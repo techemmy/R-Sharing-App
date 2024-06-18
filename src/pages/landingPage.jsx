cb31958468907fa98726ea2ecd1804eafb91e0bb
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-18 08:39:19 +0100

    refactor: redesign landing page

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
-import Registration from "./signupForm";
 
-const LandingPage = () => {
+export default function LandingPage() {
   return (
-    <div>
-      <h1>Welcome</h1>
-      <h1>This is the Landing Page</h1>
-      <Link to={"/login"}>Login</Link>
-      <Link to={"/signup"}>Register</Link>
-    </div>
+    <header className="relative h-screen flex justify-center items-center">
+      <img src="https://uploads-ssl.webflow.com/646f65e37fe0275cfb808405/646f66cdeeb4ddfdae25a26e_Background%20Hero.svg" alt="" className="absolute -z-10 inline-block h-full w-full object-cover" />
+      <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-24 lg:py-32">
+        <div className="mx-auto max-w-3xl text-center">
+          <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text mb-6 pb-4 text-4xl font-bold text-transparent md:text-6xl">Compass, Literally Go The Extra Miles in your Studies</h1>
+          <p className="mx-auto mb-5 max-w-[528px] text-xl text-[#636262] lg:mb-8">Your Study Navigator</p>
+          <Link to={'/signup'} className="inline-block rounded-full bg-blue-500 px-8 py-4 text-center font-bold text-black transition hover:border-black hover:bg-white">Get Started</Link>
+          <Link to={'/home'} className="ml-2 inline-block rounded-full  px-8 py-4 text-center font-bold text-white transition border-white bg-transparent hover:text-black hover:bg-white">Continue</Link>
+        </div>
+      </div>
+    </header>
   );
 };
-
-export default LandingPage;

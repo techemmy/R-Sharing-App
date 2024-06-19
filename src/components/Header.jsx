49de5e8c9176d9fb8ac3c665a1d580d62fe014ea
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-19 08:56:23 +0100

    fix: remove nested link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -84,9 +84,7 @@ export default function Header({ view = headerViews.Home }) {
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="text-center sm:text-left">
-            <Link to={'/home'}>
-              <Logo />
-            </Link>
+            <Logo />
             <p className="mt-1.5 text-lg text-gray-500">Welcome, {user.username}! 🎉</p>
           </div>
 

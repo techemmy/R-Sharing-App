e33e63deaec3592927ffce5cffad7f42fe984375
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-19 13:41:58 +0100

    refactor: change cors options

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,10 @@ import { HttpExceptionFilter } from './http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://acrid-hour-snotty-hand-production.pipeops.app',
-    ],
-    credentials: true,
-    allowedHeaders: '*',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   app.setGlobalPrefix('/api');
   app.enableVersioning({ type: VersioningType.URI });

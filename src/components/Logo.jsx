202b73b77609a4d480d8a34506ddc04a1b3a473d
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-19 08:16:38 +0100

    fix: Link logo to homepage

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,3 +1,9 @@
+import { Link } from 'react-router-dom';
+
 export default function Logo() {
-  return <h1 className="bg-gradient-to-r from-green-300 via-indigo-400 to-purple-800 bg-clip-text mb-6 pb-4 text-4xl font-bold text-transparent md:text-6xl">Compass</h1>
+  return (
+    <Link to={'/'}>
+      <h1 className="bg-gradient-to-r from-green-300 via-indigo-400 to-purple-800 bg-clip-text text-4xl font-bold text-transparent md:text-6xl">Compass</h1>
+    </Link>
+  )
 }

0b081d72db43733a8a05e30f0aa6b70cec4267a8
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-06-28 11:44:36 +0100

    refactor: remove unnecessary code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { AuthService } from './auth.service';
 import { LogInDto } from './dto/login.dto';
-import { RegisterDto } from './dto/register.dto';
 
 @Controller({ version: '1', path: 'auth' })
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/register')
-  async register(@Body() registerDto: RegisterDto) {
+  async register(@Body() registerDto: CreateUserDto) {
     return this.authService.register(registerDto);
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,8 +2,8 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import { LogInDto } from './dto/login.dto';
-import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +12,7 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async register(registerDto: RegisterDto) {
+  async register(registerDto: CreateUserDto) {
     const { password: _, ...user } = (
       await this.usersService.create(registerDto)
     ).toJSON();

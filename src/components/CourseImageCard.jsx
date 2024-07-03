97ed0d0cd73e4f6d37b227244b47ae60ba827613
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-03 17:36:28 +0100

    refactor: change image view to Carousel with download button

--- src/components/CourseImageCard.jsx
+++ src/components/CourseImageCard.jsx
@@ -1,3 +1,5 @@
+import { CarouselItem } from "@/components/ui/carousel";
+
 async function downloadImage(imageSrc, imageName) {
   const image = await fetch(imageSrc)
   const imageBlog = await image.blob()
@@ -11,17 +13,44 @@ async function downloadImage(imageSrc, imageName) {
   document.body.removeChild(link)
 }
 
-export default function ResourceImageCard({
+export default function CourseImageCard({
   image,
-  imageDownloadFilename,
+  courseInfo,
 
 }) {
   return (
+    <CarouselItem className="relative">
+      <img src={image.url} alt={`${courseInfo} page - ${image.pageNo}`} className="object-cover w-full h-auto rounded-lg" />
+      <button
+        onClick={() => {
+          downloadImage(image.url, `${courseInfo}-${image.pageNo}`)
+        }}
+        className="absolute bottom-2 right-2 bg-gray-800 text-white rounded-full p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
+          />
+        </svg>
+        <span className="sr-only">Download</span>
+      </button>
+    </CarouselItem>
+  )
+  {/* return (
     <div className="w-[200px] h-auto rounded-lg overflow-hidden shrink-0 relative" >
       <img src={image.url} alt="Resource Image 1" className="w-full h-auto object-cover" />
       <button
         onClick={() => {
-          downloadImage(image.url, imageDownloadFilename)
+          downloadImage(image.url, courseInfo)
         }}
         className="absolute bottom-2 right-2 bg-gray-800 text-white rounded-full p-2 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
       >
@@ -43,4 +72,5 @@ export default function ResourceImageCard({
       </button>
     </div >
   )
+  */}
 }

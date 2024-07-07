21034848496afe39013fba0d2e59e00f86d11bf3
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-07 15:20:28 +0100

    fix: correct resource view link

--- src/components/ResourceCards.jsx
+++ src/components/ResourceCards.jsx
@@ -4,7 +4,7 @@ import Card from "./Card";
 export default function ResourceCards({ resources }) {
   const resourceCards = resources.map((resource) => {
     return (
-      <Link to={`/view-resource/${resource._id}`} key={`${resource._id}-${resource.courseName}`}>
+      <Link to={`/view-course/${resource._id}`} key={`${resource._id}-${resource.courseName}`}>
         <Card resource={resource} />
       </Link >
     )

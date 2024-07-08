b0c23cd42b6cbcbfed0409d148988ae5dfe37611
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-08 23:45:56 +0100

    refactor: remove unused functions

--- src/constants.js
+++ src/constants.js
@@ -4,20 +4,6 @@ export const RESOURCE_TYPE = {
   Notes: "NOTE",
 };
 
-export const RESOURCES_FILTER = {
-  all: (resources) => resources,
-  notes: (resources) =>
-    resources.filter(
-      (resource) => resource.resourceType.toLowerCase() === RESOURCE_TYPE.Notes,
-    ),
-  pq: (resources) =>
-    resources.filter(
-      (resource) =>
-        resource.resourceType?.toLowerCase() ===
-        RESOURCE_TYPE["Past Questions"],
-    ),
-};
-
 export const headerViews = {
   Home: "home",
   CreateResource: "create-resource",

6d4afdd6c166adb65e450c1c86c0a368bd7a40d9
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-09 13:57:32 +0100

    fix: remove duplicate git Button.jsx file

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -3,7 +3,6 @@ import { Slot } from "@radix-ui/react-slot"
 import { cva } from "class-variance-authority";
 
 import { cn } from "@/lib/utils"
-import { Loader } from "lucide-react";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -34,22 +33,13 @@ const buttonVariants = cva(
   }
 )
 
-const Button = React.forwardRef(({ className, variant, size, asChild = false, isLoading, loaderColor, children, ...props }, ref) => {
+const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
   const Comp = asChild ? Slot : "button"
-  const loader = (
-    <>
-      <Loader className="mr-2 h-4 w-4 animate-spin" />
-      Please wait
-    </>
-  )
   return (
     (<Comp
       className={cn(buttonVariants({ variant, size, className }))}
       ref={ref}
-      {...props} >
-
-      {isLoading ? loader : children}
-    </Comp>)
+      {...props} />)
   );
 })
 Button.displayName = "Button"

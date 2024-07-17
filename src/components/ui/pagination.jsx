0fddd3932a9604da90bf7256c73d6ae2370476a5
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-17 19:02:30 +0100

    style: change active pagination item style

--- src/components/ui/pagination.jsx
+++ src/components/ui/pagination.jsx
@@ -38,7 +38,7 @@ const PaginationLink = ({
   <a
     aria-current={isActive ? "page" : undefined}
     className={cn(buttonVariants({
-      variant: isActive ? "outline" : "ghost",
+      variant: isActive ? "secondary" : "ghost",
       size,
     }), className)}
     {...props} />

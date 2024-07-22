9a65ea83e2a18e9e452a56d02a1c676e6a1b2700
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-07-22 20:41:15 +0100

    feat: add logout btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,39 @@ import { HEADER_VIEWS } from "../constants";
 import useAuth from "../hooks/useAuth"
 import Logo from './Logo'
 import ButtonAndLinkLoader from "./ButtonAndLinkLoader";
+import { Button } from "./ui/button";
 
 export default function Header({ view = HEADER_VIEWS.Home }) {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
   const navigate = useNavigate();
   const navigation = useNavigation();
 
   const homeBtnView = (
     <>
+      <Button
+        className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-gray-200 bg-white px-5 py-3 text-gray-500 transition hover:text-gray-700 focus:outline-none focus:ring"
+        type="button"
+        variant="outline"
+        onClick={logout}
+      >
+        <span className="text-sm font-medium">Logout</span>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+          />
+        </svg>
+      </Button>
+
       <button
         className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-gray-200 bg-white px-5 py-3 text-gray-500 transition hover:text-gray-700 focus:outline-none focus:ring cursor-not-allowed"
         type="button"

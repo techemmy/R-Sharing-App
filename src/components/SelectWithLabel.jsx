67ead5f93ac70657fc19d7f11fa8a4baf977a463
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-03 22:30:31 +0100

    fix: remove errors

--- src/components/SelectWithLabel.jsx
+++ src/components/SelectWithLabel.jsx
@@ -19,15 +19,12 @@ export default function SelectWithLabel({
             id={name}
             className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm py-2 px-3"
             name={name}
+            value={value}
             {...props}
           >
-            <option>Select school</option>
+            <option>Select one</option>
             {options.map((option) => (
-              <option
-                key={option.value}
-                value={option.value}
-                selected={value === option.value}
-              >
+              <option key={option.value} value={option.value}>
                 {option.name}
               </option>
             ))}

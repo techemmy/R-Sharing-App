e72d3fa9a3461ef9e98c713881be4c586dbea4b3
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-03 22:02:16 +0100

    feat: Create custom input and select components with labels

---
+++ src/components/InputWithLabel.jsx
@@ -0,0 +1,19 @@
+export default function InputWithLabel({ name, label, placeholder, ...props }) {
+  return (
+    <div>
+      <label htmlFor={name} className="block text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          required
+          id={name}
+          placeholder={placeholder}
+          className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm py-2 px-3"
+          name={name}
+          {...props}
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/SelectWithLabel.jsx
@@ -0,0 +1,39 @@
+export default function SelectWithLabel({
+  name,
+  label,
+  value,
+  options,
+  ...props
+}) {
+  return (
+    <div>
+      <label
+        htmlFor={name}
+        className="block text-sm font-medium text-gray-700 "
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <div className="relative">
+          <select
+            id={name}
+            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm py-2 px-3"
+            name={name}
+            {...props}
+          >
+            <option>Select school</option>
+            {options.map((option) => (
+              <option
+                key={option.value}
+                value={option.value}
+                selected={value === option.value}
+              >
+                {option.name}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+    </div>
+  );
+}

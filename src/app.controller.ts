3040b89d25779e44a768c337eb44f56f83ca2106
Emmanuel Oloyede <emmanueltopea@gmail.com>
2024-08-07 05:11:23 +0100

    feat: add endpoint to get countries

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AfricanCountriesAndTerritories } from './users/enums/country.enum';
 
-@Controller({ version: '1'})
+@Controller({ version: '1' })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/countries')
+  getCountries() {
+    return AfricanCountriesAndTerritories;
   }
 }
